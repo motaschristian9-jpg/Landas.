d04ca6768aba8deb70d0435f2cd24d37e8d1aaad
ChristianOrlanMotas <motas.christian9@gmail.com>
2026-05-11 19:56:24 +0800

    chore: remove has_seen_tutorial column from users table

--- database/migrations/2026_05_11_102101_remove_has_seen_tutorial_from_users_table.php
+++ database/migrations/2026_05_11_102101_remove_has_seen_tutorial_from_users_table.php
@@ -4,14 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            if (Schema::hasColumn('users', 'has_seen_tutorial')) {
+                $table->dropColumn('has_seen_tutorial');
+            }
         });
     }
 
@@ -21,7 +24,7 @@ return new class extends Migration {
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->boolean('has_seen_tutorial')->default(false)->after('email');
         });
     }
 };
